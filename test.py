4308c9408aaf9aee02515d9faa17a102154f8ec1
A3M_ILS <ilyass.aamoum2004@gmail.com>
2026-02-21 18:49:24 +0100

    Project structure

--- test.py
+++
@@ -1,9 +0,0 @@
-import torch, time
-
-x = torch.randn(6000, 6000, device="cuda")
-y = torch.randn(6000, 6000, device="cuda")
-
-t0 = time.time()
-z = x @ y
-torch.cuda.synchronize()
-print("Time:", time.time() - t0)
